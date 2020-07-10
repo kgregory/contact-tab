3cce8fe81cc51a59672b9ab0f34f54a59a9133c2
Ken Gregory <kgreg80@gmail.com>
2020-07-10 14:40:51 -0400

    Add delete progress to assignee chips

--- src/AssigneesCard.js
+++ src/AssigneesCard.js
@@ -4,6 +4,7 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import Chip from "@material-ui/core/Chip";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles(theme => ({
   chip: {
@@ -15,6 +16,14 @@ export default function AssigneesCard({
   classes: overrideClasses = { root: undefined }
 }) {
   const classes = useStyles();
+  const [deleting, setDeleting] = React.useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ]);
 
   const assignees = [
     "Shawn McKnight",
@@ -25,12 +34,24 @@ export default function AssigneesCard({
     "Glenn Ryan"
   ];
 
+  const handleDelete = index => () => {
+    const newDeleting = deleting.slice();
+    newDeleting[index] = !newDeleting[index];
+    setDeleting(newDeleting);
+  };
+
   return (
     <Card className={overrideClasses.root}>
       <CardHeader title="Assigned Users" />
       <CardContent>
-        {assignees.map(name => (
-          <Chip className={classes.chip} label={name} onDelete={() => {}} />
+        {assignees.map((name, index) => (
+          <Chip
+            className={classes.chip}
+            label={name}
+            onDelete={handleDelete(index)}
+            disabled={deleting[index]}
+            deleteIcon={deleting[index] && <CircularProgress size={22} />}
+          />
         ))}
       </CardContent>
     </Card>
